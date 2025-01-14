e3080db02410efdda426a8bcd7270a1c52ac6ea0
colinjones26 <colinanthonyjones1996@gmail.com>
2025-01-14 15:41:17 -0500

    Initial Commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        //Private field
+        private double balance = 0;
+
+        //Deposit method
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Instance of the BankAccount class
+            BankAccount account = new BankAccount();
+
+            // User to deposit money
+            Console.Write("Enter an amount to deposit: ");
+            string input = Console.ReadLine();
+
+            // Input can convert to a double
+            bool isValidAmount = double.TryParse(input, out double depositAmount);
+
+            // If the amount is valid and greater than 0
+            if (isValidAmount && depositAmount > 0)
+            {
+                account.Deposit(depositAmount);
+                Console.WriteLine($"You have successfully deposited: {depositAmount:C}.");
+            }
+            else
+            {
+                // Invalid input or non-positive amount
+                Console.WriteLine("Invalid input. Please enter a valid positive number.");
+            }
+
+            // User can see the balance
+            double balance = account.GetBalance();
+            Console.WriteLine($"Your current balance is: {balance:C}.");
         }
     }
 }
